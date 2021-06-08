c0c9c83063bd4e2471589023de0ad6c0cbe0a5f6
Ido Shamun <idoesh1@gmail.com>
2021-06-08 21:21:26 +0300

    build: version 1.0.1

--- lib/cloudRun.d.ts
+++ lib/cloudRun.d.ts
@@ -10,7 +10,7 @@ export declare enum CloudRunAccess {
 }
 export declare function createCloudRunService(name: string, image: string, envs: Input<Input<ServiceTemplateSpecContainerEnv>[]>, limits: Input<{
     [key: string]: Input<string>;
-}>, vpcConnector: gcp.vpcaccess.Connector, serviceAccount: gcp.serviceaccount.Account, opts?: {
+}>, vpcConnector: gcp.vpcaccess.Connector | Output<gcp.vpcaccess.Connector>, serviceAccount: gcp.serviceaccount.Account | Output<gcp.serviceaccount.Account>, opts?: {
     minScale?: number;
     concurrency?: number;
     dependsOn?: Input<Input<Resource>[]>;
