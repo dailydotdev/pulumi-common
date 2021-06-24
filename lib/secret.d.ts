dd7f6d3e7a30a5620d052ef810628d48157b606c
Ido Shamun <idoesh1@gmail.com>
2021-06-24 18:26:31 +0300

    feat: add createEncryptedEnvVar utility function

--- lib/secret.d.ts
+++ lib/secret.d.ts
@@ -4,4 +4,5 @@ export declare type Secret = {
     name: string;
     value: string | Output<string>;
 };
+export declare function createEncryptedEnvVar(prefix: string, key: string, value: string | Output<string>): Secret;
 export declare function createEnvVarsFromSecret(prefix: string): pulumi.Input<Secret>[];
--- src/secret.ts
+++ src/secret.ts
@@ -6,29 +6,38 @@ import { config } from './config';
 
 export type Secret = { name: string; value: string | Output<string> };
 
+export function createEncryptedEnvVar(
+  prefix: string,
+  key: string,
+  value: string | Output<string>,
+): Secret {
+  const secret = new gcp.secretmanager.Secret(`${prefix}-secret-${key}`, {
+    secretId: `${prefix}-secret-${key}`,
+    replication: { automatic: true },
+  });
+
+  const version = new gcp.secretmanager.SecretVersion(`${prefix}-sv-${key}`, {
+    enabled: true,
+    secret: secret.name,
+    secretData: value,
+  });
+  return {
+    name: camelToUnderscore(key),
+    value: pulumi
+      .all([secret.secretId, version.id])
+      .apply(
+        ([name, version]) =>
+          `gcp:///${name}/${version.split('/').reverse()[0]}`,
+      ),
+  };
+}
+
 export function createEnvVarsFromSecret(
   prefix: string,
 ): pulumi.Input<Secret>[] {
   const envVars = config.requireObject<Record<string, string>>('env');
-  return Object.keys(envVars).map((key): pulumi.Input<Secret> => {
-    const secret = new gcp.secretmanager.Secret(`${prefix}-secret-${key}`, {
-      secretId: `${prefix}-secret-${key}`,
-      replication: { automatic: true },
-    });
-
-    const version = new gcp.secretmanager.SecretVersion(`${prefix}-sv-${key}`, {
-      enabled: true,
-      secret: secret.name,
-      secretData: envVars[key],
-    });
-    return {
-      name: camelToUnderscore(key),
-      value: pulumi
-        .all([secret.secretId, version.id])
-        .apply(
-          ([name, version]) =>
-            `gcp:///${name}/${version.split('/').reverse()[0]}`,
-        ),
-    };
-  });
+  return Object.keys(envVars).map(
+    (key): pulumi.Input<Secret> =>
+      createEncryptedEnvVar(prefix, key, envVars[key]),
+  );
 }
