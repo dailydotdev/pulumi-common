6823200f70f12277228ce1b981b3c0d3f022b115
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-05-02 14:37:42 +0200

    fix: add missing resourceOptions to helm release for Redis (#315)

--- src/resources/redis/kubernetesRedis.ts
+++ src/resources/redis/kubernetesRedis.ts
@@ -19,9 +19,9 @@ export class KubernetesRedis extends pulumi.ComponentResource {
   constructor(
     name: string,
     args: K8sRedisArgs,
-    opts?: pulumi.CustomResourceOptions,
+    resourceOptions?: pulumi.CustomResourceOptions,
   ) {
-    super(`${urnPrefix}:KubernetesRedis`, name, args, opts);
+    super(`${urnPrefix}:KubernetesRedis`, name, args, resourceOptions);
 
     const redisInstance = {
       persistence: configurePersistence({
@@ -35,46 +35,50 @@ export class KubernetesRedis extends pulumi.ComponentResource {
       }),
     };
 
-    new k8s.helm.v3.Release(name, {
-      ...charts['redis'],
-      namespace: args.namespace,
-      createNamespace: false,
-      atomic: true,
-      values: {
-        fullnameOverride: name,
-        commonConfiguration: commonConfiguration,
-        image: pulumi.all([args.image]).apply(([image]) => ({
-          repository: image?.repository || defaultImage.repository,
-          tag: image?.tag || defaultImage.tag,
-        })),
-        metrics: pulumi.all([args.metrics]).apply(([metrics]) => ({
-          ...metrics,
-          enabled: metrics?.enabled ?? true,
-          resourcesPreset: 'micro',
-        })),
+    new k8s.helm.v3.Release(
+      name,
+      {
+        ...charts['redis'],
+        namespace: args.namespace,
+        createNamespace: false,
+        atomic: true,
+        values: {
+          fullnameOverride: name,
+          commonConfiguration: commonConfiguration,
+          image: pulumi.all([args.image]).apply(([image]) => ({
+            repository: image?.repository || defaultImage.repository,
+            tag: image?.tag || defaultImage.tag,
+          })),
+          metrics: pulumi.all([args.metrics]).apply(([metrics]) => ({
+            ...metrics,
+            enabled: metrics?.enabled ?? true,
+            resourcesPreset: 'micro',
+          })),
 
-        // Values specific to master-slave setup
-        architecture: args.architecture || 'replication',
-        auth: {
-          enabled: !!args.authKey,
-          password: args.authKey,
+          // Values specific to master-slave setup
+          architecture: args.architecture || 'replication',
+          auth: {
+            enabled: !!args.authKey,
+            password: args.authKey,
+          },
+          master: pulumi
+            .all([args.affinity, args.tolerations])
+            .apply(([affinity, tolerations]) => ({
+              ...redisInstance,
+              affinity: affinity?.master,
+              tolerations: tolerations?.master,
+            })),
+          replica: pulumi
+            .all([args.affinity, args.tolerations])
+            .apply(([affinity, tolerations]) => ({
+              ...redisInstance,
+              replicaCount: args.replicas || 3,
+              affinity: affinity?.replicas,
+              tolerations: tolerations?.replicas,
+            })),
         },
-        master: pulumi
-          .all([args.affinity, args.tolerations])
-          .apply(([affinity, tolerations]) => ({
-            ...redisInstance,
-            affinity: affinity?.master,
-            tolerations: tolerations?.master,
-          })),
-        replica: pulumi
-          .all([args.affinity, args.tolerations])
-          .apply(([affinity, tolerations]) => ({
-            ...redisInstance,
-            replicaCount: args.replicas || 3,
-            affinity: affinity?.replicas,
-            tolerations: tolerations?.replicas,
-          })),
       },
-    });
+      resourceOptions,
+    );
   }
 }
