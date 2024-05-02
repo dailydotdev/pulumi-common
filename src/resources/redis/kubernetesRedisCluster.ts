0bda55606ac149192cb2bdbf6f54264fb1cb43df
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-05-02 14:35:15 +0200

    fix: add missing resourceOptions to helm release for Redis

--- src/resources/redis/kubernetesRedisCluster.ts
+++ src/resources/redis/kubernetesRedisCluster.ts
@@ -19,48 +19,52 @@ export class KubernetesRedisCluster extends pulumi.ComponentResource {
   constructor(
     name: string,
     args: K8sRedisClusterArgs,
-    opts?: pulumi.CustomResourceOptions,
+    resourceOptions?: pulumi.CustomResourceOptions,
   ) {
-    super(`${urnPrefix}:KubernetesRedisCluster`, name, args, opts);
+    super(`${urnPrefix}:KubernetesRedisCluster`, name, args, resourceOptions);
 
-    new k8s.helm.v3.Release(name, {
-      ...charts['redis-cluster'],
-      namespace: args.namespace,
-      createNamespace: false,
-      atomic: true,
-      values: {
-        fullnameOverride: name,
-        commonConfiguration: commonConfiguration,
-        image: pulumi.all([args.image]).apply(([image]) => ({
-          repository: image?.repository || defaultImage.repository,
-          tag: image?.tag || defaultImage.tag,
-        })),
-        metrics: pulumi.all([args.metrics]).apply(([metrics]) => ({
-          ...metrics,
-          enabled: metrics?.enabled ?? true,
-          resourcesPreset: 'micro',
-        })),
+    new k8s.helm.v3.Release(
+      name,
+      {
+        ...charts['redis-cluster'],
+        namespace: args.namespace,
+        createNamespace: false,
+        atomic: true,
+        values: {
+          fullnameOverride: name,
+          commonConfiguration: commonConfiguration,
+          image: pulumi.all([args.image]).apply(([image]) => ({
+            repository: image?.repository || defaultImage.repository,
+            tag: image?.tag || defaultImage.tag,
+          })),
+          metrics: pulumi.all([args.metrics]).apply(([metrics]) => ({
+            ...metrics,
+            enabled: metrics?.enabled ?? true,
+            resourcesPreset: 'micro',
+          })),
 
-        usePassword: !!args.authKey,
-        password: args.authKey,
-        cluster: {
-          nodes: args.nodes,
-          replicas: args.replicas,
-        },
-        redis: {
-          resources: configureResources({
-            isAdhocEnv: args.isAdhocEnv,
+          usePassword: !!args.authKey,
+          password: args.authKey,
+          cluster: {
+            nodes: args.nodes,
+            replicas: args.replicas,
+          },
+          redis: {
+            resources: configureResources({
+              isAdhocEnv: args.isAdhocEnv,
+              memorySizeGb: args.memorySizeGb,
+              cpuSize: args.cpuSize,
+            }),
+            affinity: args.affinity,
+            tolerations: args.tolerations,
+          },
+          persistence: configurePersistence({
             memorySizeGb: args.memorySizeGb,
-            cpuSize: args.cpuSize,
+            persistence: args.persistence,
           }),
-          affinity: args.affinity,
-          tolerations: args.tolerations,
         },
-        persistence: configurePersistence({
-          memorySizeGb: args.memorySizeGb,
-          persistence: args.persistence,
-        }),
       },
-    });
+      resourceOptions,
+    );
   }
 }
