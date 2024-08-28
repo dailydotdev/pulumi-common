c6c22078032f0dc83051fe402bd8ad3b100f68af
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-28 19:26:03 +0200

    chore: update redis to 7.4

--- src/kubernetes/charts.ts
+++ src/kubernetes/charts.ts
@@ -1,14 +1,14 @@
 export const charts = {
   redis: {
     chart: 'redis',
-    version: '19.6.4',
+    version: '20.0.3',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
   },
   'redis-cluster': {
     chart: 'redis-cluster',
-    version: '10.3.0',
+    version: '11.0.3',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
