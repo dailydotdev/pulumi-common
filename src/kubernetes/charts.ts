b8622e1ba56b6f1c13574792296714cabca3367c
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-28 19:24:26 +0200

    chore: bump redis chart versions

--- src/kubernetes/charts.ts
+++ src/kubernetes/charts.ts
@@ -1,14 +1,14 @@
 export const charts = {
   redis: {
     chart: 'redis',
-    version: '19.0.2',
+    version: '19.6.4',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
   },
   'redis-cluster': {
     chart: 'redis-cluster',
-    version: '10.0.2',
+    version: '10.3.0',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
