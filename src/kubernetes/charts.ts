8014bceebf5854d7a7ea6c062e7f47c7ae8897d3
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-08-29 10:03:15 +0200

    chore: bump redis charts and upgrade redis from `v7.2` to `v7.4` (#391)

--- src/kubernetes/charts.ts
+++ src/kubernetes/charts.ts
@@ -1,14 +1,14 @@
 export const charts = {
   redis: {
     chart: 'redis',
-    version: '19.0.2',
+    version: '20.0.3',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
   },
   'redis-cluster': {
     chart: 'redis-cluster',
-    version: '10.0.2',
+    version: '11.0.3',
     repositoryOpts: {
       repo: 'https://charts.bitnami.com/bitnami',
     },
