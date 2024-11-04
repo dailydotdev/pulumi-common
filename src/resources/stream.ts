630e1adb5c55c0d8e56de752d85b42ef4ba6fd78
Ido Shamun <idoesh1@gmail.com>
2024-11-04 22:58:22 +0200

    refactor: don't use custom resource for stream and subscription

--- src/resources/stream.ts
+++ src/resources/stream.ts
@@ -1,6 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as gcp from '@pulumi/gcp';
-import { urnPrefix } from '../constants';
 import {
   PubsubEmulatorSubscription,
   PubsubEmulatorTopic,
@@ -8,26 +7,20 @@ import {
 
 export type StreamArgs = gcp.pubsub.TopicArgs & { isAdhocEnv: boolean };
 
-export class Stream extends pulumi.ComponentResource {
-  private readonly instance: gcp.pubsub.Topic | undefined;
+export class Stream {
   private readonly topicName: pulumi.Input<string>;
+  public readonly resource: pulumi.Resource;
 
   constructor(
     name: string,
     args: StreamArgs,
     opts?: pulumi.CustomResourceOptions,
   ) {
-    super(`${urnPrefix}:Stream`, name, args, opts);
-
     this.topicName = args.name as pulumi.Input<string>;
     if (args.isAdhocEnv) {
-      new PubsubEmulatorTopic(name, args, { ...opts, parent: this });
+      this.resource = new PubsubEmulatorTopic(name, args, opts);
     } else {
-      this.instance = new gcp.pubsub.Topic(name, args, {
-        ...opts,
-        parent: this,
-        aliases: [{ name, parent: pulumi.rootStackResource }],
-      });
+      this.resource = new gcp.pubsub.Topic(name, args, opts);
     }
   }
 
@@ -40,28 +33,22 @@ export type StreamSubscriptionArgs = gcp.pubsub.SubscriptionArgs & {
   isAdhocEnv: boolean;
 };
 
-export class StreamSubscription extends pulumi.ComponentResource {
-  private instance: gcp.pubsub.Subscription | undefined;
-  private subName: pulumi.Input<string>;
-  private topicName: pulumi.Input<string>;
+export class StreamSubscription {
+  private readonly subName: pulumi.Input<string>;
+  private readonly topicName: pulumi.Input<string>;
+  public readonly resource: pulumi.Resource;
 
   constructor(
     name: string,
     args: StreamSubscriptionArgs,
     opts?: pulumi.CustomResourceOptions,
   ) {
-    super(`${urnPrefix}:StreamSubscription`, name, args, opts);
-
     this.subName = args.name as pulumi.Input<string>;
     this.topicName = args.topic as pulumi.Input<string>;
     if (args.isAdhocEnv) {
-      new PubsubEmulatorSubscription(name, args, { ...opts, parent: this });
+      this.resource = new PubsubEmulatorSubscription(name, args, opts);
     } else {
-      this.instance = new gcp.pubsub.Subscription(name, args, {
-        ...opts,
-        parent: this,
-        aliases: [{ name, parent: pulumi.rootStackResource }],
-      });
+      this.resource = new gcp.pubsub.Subscription(name, args, opts);
     }
   }
 
