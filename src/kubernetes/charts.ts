0805e8f947e57fe6f802cf79746e5c5f86d162d9
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2025-04-23 13:26:28 +0200

    fix: specify `index.yaml` in repo url

--- src/kubernetes/charts.ts
+++ src/kubernetes/charts.ts
@@ -1,16 +1,18 @@
-export const charts = {
+import type { ReleaseArgs } from '@pulumi/kubernetes/helm/v3';
+
+export const charts: Record<string, ReleaseArgs> = {
   redis: {
     chart: 'redis',
     version: '20.2.1',
     repositoryOpts: {
-      repo: 'https://charts.bitnami.com/bitnami',
+      repo: 'https://charts.bitnami.com/bitnami/index.yaml',
     },
   },
   'redis-cluster': {
     chart: 'redis-cluster',
     version: '11.0.6',
     repositoryOpts: {
-      repo: 'https://charts.bitnami.com/bitnami',
+      repo: 'https://charts.bitnami.com/bitnami/index.yaml',
     },
   },
 } as const;
