2bce921101b46bd17cf913ce0ae503089de6cd99
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-01-26 18:44:12 +0100

    fix: move config for redis-cluster to proper value

--- src/resources/redis/kubernetesRedisCluster.ts
+++ src/resources/redis/kubernetesRedisCluster.ts
@@ -33,11 +33,6 @@ export class KubernetesRedisCluster extends pulumi.ComponentResource {
         namespace: args.namespace,
         values: {
           fullnameOverride: name,
-          commonConfiguration: configureConfiguration({
-            modules: args.modules,
-            configuration: args.configuration,
-            memorySizeGb: args.memorySizeGb,
-          }),
           commonAnnotations: {
             'cluster-autoscaler.kubernetes.io/safe-to-evict':
               args?.safeToEvict?.valueOf() ?? 'false',
@@ -78,6 +73,12 @@ export class KubernetesRedisCluster extends pulumi.ComponentResource {
             affinity: args.affinity,
             tolerations: args.tolerations,
             priorityClassName: configurePriorityClass(args),
+            configmap: configureConfiguration({
+              modules: args.modules,
+              configuration: args.configuration,
+              configurationOld: args.configurationOld,
+              memorySizeGb: args.memorySizeGb,
+            }),
           },
           persistence: configurePersistence({
             memorySizeGb: args.memorySizeGb,
